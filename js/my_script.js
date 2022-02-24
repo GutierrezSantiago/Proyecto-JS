8224a198969193f449a23efa9ae1cc9d54556dd5
Santiago Gutierrez <santi0gutierrez0@gmail.com>
2022-02-23 23:02:22 -0300

    Agregue un prompt para testear una idea y un menu de opciones para probar las funciones de imc y oneRepMax

--- js/my_script.js
+++ js/my_script.js
@@ -56,4 +56,29 @@ function sportSuggest(){
 let test = 0
 while(test == 0){
     sportSuggest()
-    test = prompt("Desea salir del prompt? (1 para si, 0 para no)")}
+    test = prompt("Desea salir del prompt de sugerencias? (1 para si, 0 para no)")}
+
+    do {
+        let opt = prompt("Calculadora fitness \n\nIngrese el número asignado a la opción que quiere realizar:\n1-Indice de Masa Corporal (IMC)\n2-Peso máximo de una repetición\n3-Salir")
+        var opcion = parseInt(opt)
+        if (opt == 1) {
+            let weightInput = prompt("Ingrese el peso en kg: ")
+            let weight = parseFloat(weightInput)
+            let heightInput = prompt("Ingrese su altura en metros: ")
+            let height = parseFloat(heightInput)
+            alert("Su indice de masa corporal es " + imc(weight, height))
+        } else if (opt == 2) {
+            let unit = prompt("¿En que unidad ingresara el peso? (0 para kilogramos, 1 para libras)")
+            let reps = prompt("Ingrese cuantas repeticiones realiza: ")
+            let weight = prompt("Ingrese cuanto peso realiza con esas repeticiones: ")
+            let repMax = oneRepMax(weight, reps)
+            if (unit == 0) {
+                alert("Su máximo estimado con una repetición es " + repMax + "kg.")
+            } else if (unit == 1) {
+                alert("Su máximo estimado con una repetición es " + repMax + "lbs.")
+            }
+
+        }
+    
+    }
+        while (opcion != 3)
